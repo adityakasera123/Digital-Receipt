6cbcf2dcb3bb16aa51d2cb49a059fea8f2df145a
Aditya <adityakasera40@gmail.com>
2026-07-07 12:00:00 +0530

    Routes Test

---
+++ client/src/routes/AppRoutes.jsx
@@ -0,0 +1,17 @@
+import { Routes, Route } from "react-router-dom";
+
+import Landing from "../pages/Landing/Landing";
+import Login from "../pages/Login/Login";
+import Signup from "../pages/Signup/Signup";
+
+function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<Landing />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+    </Routes>
+  );
+}
+
+export default AppRoutes;
