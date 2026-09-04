335eaec8ec8acf591af317247bede8ed9badaa9b
Aditya <adityakasera40@gmail.com>
2026-09-04 20:28:06 +0530

    fix: support GitHub Pages base route

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -8,7 +8,7 @@ import ThemeProvider from './context/ThemeContext';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Digital-Receipt">
       <ThemeProvider>
         <AuthProvider>
           <App />
